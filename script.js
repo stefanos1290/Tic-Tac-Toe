64a681cea3d4ad4ac08497397ed993376086ed8c
stefanos <stefanostrr@hotmail.com>
2020-11-19 15:27:00 +0100

    place mark function

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
+const X_CLASS = 'x';
+const CIRCLE_CLASS = 'circle';
 const cellElements = document.querySelectorAll('[data-cell]');
+let circleTurn
 
 cellElements.forEach(cell => {
     cell.addEventListener('click', hendleClick, { once: true})
 });
 
 function hendleClick(e) {
+    const cell = e.target;
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+    placeMark(cell, currentClass)
     console.log(e)
+}
+
+function placeMark(cell, currentClass){
+    cell.classList.add(currentClass)
 }
